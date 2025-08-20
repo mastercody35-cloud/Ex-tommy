e6d7dab9da3d4a6c7c5fabf8904a16829ac8f385
mastercody35-cloud <mastercody35@gmail.com>
2025-08-20 11:10:07 +0500

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "inf",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "Siizz + edited by tOomy【☬☠𝔹𝔸𝕃𝕆ℂℍ™∆☠︎☬】",
+  credits: "Siizz + edited by tØxīīÇ bØīī【☬☠𝔹𝔸𝕃𝕆ℂℍ™∆☠︎☬",
   description: "Admin and Bot info.",
   commandCategory: "...",
   cooldowns: 1,
@@ -33,13 +33,13 @@ module.exports.run = async function({ api, event, args, client, Users, Threads, 
 ╰─❍❍❍❍❍❍❍❍❍❍❍❍─╯
 
 🌸 𝗕𝗢𝗧 𝗡𝗔𝗠𝗘: ${global.config.BOTNAME}
-👑 𝗢𝗪𝗡𝗘𝗥: Ex TOomy (Credit)
+👑 𝗢𝗪𝗡𝗘𝗥: tØxīīÇ bØīī 🤖🥰(Credit)
 💘 𝗥𝗘𝗟𝗔𝗧𝗜𝗢𝗡𝗦𝗛𝗜𝗣:
 ʜᴀɪ ᴍᴇʀɪ ᴇᴋ ᴘʏᴀʀɪ ꜱɪ ꜱʜᴇʜᴢᴀᴅɪ 💞
 ᴊɪꜱᴇ ᴍᴀɪɴ ᴘʏᴀʀ ꜱᴇ ᴊᴀɴᴀ ᴋᴇʜᴛᴀ 💗
 
 🌐 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞:
-📎 https://www.facebook.com/share/1BEmWChftj/
+📎 https://www.facebook.com/profile.php?id=61579433406440&mibextid=ZbWKwL
 
 🛠️ 𝗣𝗥𝗘𝗙𝗜𝗫: ${global.config.PREFIX}
 ⏳ 𝗨𝗣𝗧𝗜𝗠𝗘: ${hours}h ${minutes}m ${seconds}s
