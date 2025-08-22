b4a9ebf9850c22893cd7dfbc5333be954fb5b89e
mastercody35-cloud <mastercody35@gmail.com>
2025-08-22 12:15:30 +0500

    Update fyt.js

--- Priyansh/commands/fyt.js
+++ Priyansh/commands/fyt.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "fyt",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "【☬☠𝔹𝔸𝕃𝕆ℂℍ™∆☠︎☬】",
     description: "War In Chatbox",
     commandCategory: "wargroup",
     usages: "[fyt]",
